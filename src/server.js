76946e141ef49ee1613a6bf3fefef1c9fbbf2d68
Giovanni <158181635+giovannig10@users.noreply.github.com>
2025-04-17 09:52:06 -0300

    feat: create collection model controller and route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import { config } from "dotenv";
 import cors from "cors"; // Importa o middleware CORS
 
 import animesRouter from "./routes/animeRoutes.js";
+import collectionRouter from "./routes/collectionRoutes.js";
+import personagemController from "./controllers/personagemController.js";
 
 config(); // Carrega variáveis de ambiente do arquivo .env
 const port = process.env.PORT || 4001; // Define a porta do servidor
@@ -14,11 +16,9 @@ app.use(cors()); // Habilita CORS para todas as rotas
 app.use(express.json()); // Parse de JSON
 
 app.use("/animes", animesRouter); // Usar as rotas de animes
+app.use("/colecoes", collectionRouter); // Usar as rotas de colecoes
 
-// Rota base para verificar se o servidor está rodando
-app.get("/", (req, res) => {
-  res.json({ message: "API de Coleção de Animes funcionando!" });
-});
+
 
 // Iniciar o servidor
 app.listen(port, () => {
